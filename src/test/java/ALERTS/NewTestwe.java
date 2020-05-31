dc19e7aceb1892982110431128414417d290eac0
Ramesh <Ramesh@LAPTOP-23D6II4D>
2020-05-31 09:35:02 +0530

    added new prog

---
+++ src/test/java/ALERTS/NewTestwe.java
@@ -0,0 +1,19 @@
+package ALERTS;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestwe {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
